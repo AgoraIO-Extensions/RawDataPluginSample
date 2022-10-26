0c6b45ddc8116c940556b126cdc270e2c00aef29
littleGnAl <tearjeaker@hotmail.com>
2022-10-26 20:09:58 +0800

    Add flutter plugin

--- frameworks/flutter/video_raw_data_example_plugin/example/android/app/src/main/kotlin/com/example/video_raw_data_example_plugin_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.video_raw_data_example_plugin_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- frameworks/flutter/video_raw_data_example_plugin/ios/Classes/VideoRawDataExamplePlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface VideoRawDataExamplePlugin : NSObject<FlutterPlugin>
-@end
