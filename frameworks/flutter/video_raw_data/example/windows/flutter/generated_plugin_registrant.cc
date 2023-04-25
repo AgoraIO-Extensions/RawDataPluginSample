661d549bc5c06d288aaf14c6149c15b13af36785
HUI <luxuhui@agora.io>
2023-04-25 13:27:46 +0800

    ci: add ci for flutter

---
+++ frameworks/flutter/video_raw_data/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <video_raw_data/video_raw_data_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  VideoRawDataPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("VideoRawDataPluginCApi"));
+}
